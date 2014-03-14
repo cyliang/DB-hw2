cec714e4a7e3f16dfcb90301fd3bae50fe8b99e9
Chih-Yung Liang <liangcy@cs.nctu.edu.tw>
2014-03-15 02:05:06 +0800

    Name & email's input fields
    Other inputs will show after finish input name & email.

--- js/regist_dialog.js
+++ js/regist_dialog.js
@@ -6,4 +6,10 @@ function prepare_regist_dialog() {
 			$("#regist_dialog #new_confirm_pwd .warning").slideUp();
 		}
 	});
+	
+	$("#regist_dialog #new_rname input, #regist_dialog #new_email input").change(function() {
+		if($("#regist_dialog #new_rname input").val() != "" && $("#regist_dialog #new_email input").val() != "") {
+			$("#regist_dialog #new_second").slideDown();
+		}
+	});
 }
