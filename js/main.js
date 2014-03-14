a5e65ef9e200e890436463867db7397913c5bfa1
Chih-Yung Liang <liangcy@cs.nctu.edu.tw>
2014-03-14 14:29:12 +0800

    Change page function
    Define all the pages by "pages"

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
+var pages = {
+	login_dialog: {
+		div_id: "login_dialog",
+		title: "使用者登入"
+	},
+	welcome: {
+		div_id: "welcome_page",
+		title: "歡迎"
+	}
+};
+
+var now_page;
+
 $(document).ready(function() {
 	prepare_login_dialog();
 });
+
+function change_page(new_page) {
+	$("#" + pages[now_page].div_id).slideUp(function() {
+		$("#" + pages[new_page].div_id).slideDown();
+	});
+	
+	$("h2").slideUp(function() {
+		$("h2").text(pages[new_page].title);
+		$("h2").slideDown();
+	});
+	
+	$("title").text("航班管理系統 - " + pages[new_page].title);
+	now_page = new_page;
+}
