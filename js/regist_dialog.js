6ffb4f1968d245ed7110ba78dec4b8e685608cfd
Chih-Yung Liang <liangcy@cs.nctu.edu.tw>
2014-03-15 02:46:31 +0800

    validate regist form

--- js/regist_dialog.js
+++ js/regist_dialog.js
@@ -21,4 +21,18 @@ function prepare_regist_dialog() {
 			}
 		}
 	});
+	
+	$("#regist_dialog > form").submit(function() {
+		event.preventDefault();
+		var email_pat = new RegExp("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$","i");
+		
+		if($("#regist_dialog #new_rname input").val() == "") {
+			alert("您尚未輸入姓名！");
+		} else if($("#regist_dialog #new_pwd input").val() != $("#regist_dialog #new_confirm_pwd input").val() || $("#regist_dialog #new_pwd input").val() == "") {
+			alert("您尚未輸入密碼或與確認密碼不一致！");
+		} else if(!email_pat.test($("#regist_dialog #new_email input").val())) {
+			alert("Email錯誤！");
+		} else {
+		}
+	});
 }
