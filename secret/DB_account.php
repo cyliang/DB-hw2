bc2113cb74dbf4996b4482a6e18df029ee5858d8
Chih-Yung Liang <liangcy@cs.nctu.edu.tw>
2014-03-15 06:56:01 +0800

    The page which define DB's secret.

---
+++ secret/DB_account.php
@@ -0,0 +1,6 @@
+<?php
+$my_DB_account["db_name"] = "your DB name";
+$my_DB_account["host"] = "your DB host";
+$my_DB_account["user"] = "your DB user";
+$my_DB_account["password"] = "your DB password";
+?>
