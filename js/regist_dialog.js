14ee0cfde5b5cb04f9fa87b16406af6078913c7b
Chih-Yung Liang <liangcy@cs.nctu.edu.tw>
2014-03-15 02:26:49 +0800

    Email's validate

--- js/regist_dialog.js
+++ js/regist_dialog.js
@@ -8,8 +8,17 @@ function prepare_regist_dialog() {
 	});
 	
 	$("#regist_dialog #new_rname input, #regist_dialog #new_email input").change(function() {
-		if($("#regist_dialog #new_rname input").val() != "" && $("#regist_dialog #new_email input").val() != "") {
-			$("#regist_dialog #new_second").slideDown();
+		var email_pat = new RegExp("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$","i");
+		
+		if($("#regist_dialog #new_email input").val() != "") {
+			if(!email_pat.test($("#regist_dialog #new_email input").val())) {
+				$("#regist_dialog #new_email .warning").slideDown();
+			} else {
+				$("#regist_dialog #new_email .warning").slideUp();
+				if($("#regist_dialog #new_rname input").val() != "") {
+					$("#regist_dialog #new_second").slideDown();
+				}
+			}
 		}
 	});
 }
