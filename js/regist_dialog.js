c708baa2255bdab5974bf3e79a231bca79a781c3
Chih-Yung Liang <liangcy@cs.nctu.edu.tw>
2014-03-15 15:15:37 +0800

    Rewrite button

--- js/regist_dialog.js
+++ js/regist_dialog.js
@@ -8,6 +8,16 @@ function prepare_regist_dialog() {
 		});
 	});
 	
+	$("#regist_dialog #new_second #new_rewrite").click(function() {
+		$("#regist_dialog #new_second").slideUp(function() {
+			$("#regist_dialog #new_first button[type=submit]").slideDown();
+		});
+		
+		$("#regist_dialog input[type!='checkbox']").val("");
+		$("#regist_dialog #admin_chk").removeAttr("checked");
+		$("#regist_dialog #new_first input").removeAttr("readonly");
+	});
+	
 	$("#regist_dialog #new_second").submit(function() {
 		event.preventDefault();
 		
