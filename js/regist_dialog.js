dd3ae36fe8e190e33ea0b696733cffc2c39c546e
Chih-Yung Liang <liangcy@cs.nctu.edu.tw>
2014-03-15 09:22:31 +0800

    Lock name & email after click continue in regist
    Set readonly

--- js/regist_dialog.js
+++ js/regist_dialog.js
@@ -1,6 +1,8 @@
 function prepare_regist_dialog() {	
 	$("#regist_dialog #new_first").submit(function() {
 		event.preventDefault();
+		$("#regist_dialog #new_first input").attr("readonly", "");
+		
 		$("#regist_dialog #new_first button[type=submit]").slideUp(function() {
 			$("#regist_dialog #new_second").slideDown();
 		});
