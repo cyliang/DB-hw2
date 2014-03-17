2ad5e18d512d0cbbf3baa192d6fe05b93df77991
Chih-Yung Liang <stu89424@gmail.com>
2014-03-17 15:41:33 +0800

    Append retry time interval to 5 seconds.

--- php/check_login.php
+++ php/check_login.php
@@ -4,6 +4,7 @@ header('Content-Type: text/event-stream');
 header('Cache-Control: no-cache');
 
 $login = new Login();
+echo "retry: 5000" . PHP_EOL;
 echo "data: " . $login . PHP_EOL . PHP_EOL;
 flush();
 ?>
