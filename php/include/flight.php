a99291169642e31e1e0a47c8db9cf7f53c89f8e5
Chih-Yung Liang <stu89424@gmail.com>
2014-03-18 14:23:15 +0800

    Fix bugs in SQL syntax (LIMIT).

--- php/include/flight.php
+++ php/include/flight.php
@@ -11,14 +11,16 @@ class Flight {
 		$sort = (isset($options['sort']) && $options['sort'] == 'date') ? "departure_date" : "id";
 		$limit = isset($options['limit']) ? $options['limit'] : 10;
 
-		$stat = $this->db->prepare("SELECT * FROM `flight_flight` ORDER BY `{$sort}` {$order} LIMIT ? , ? ;");
-		$stat->execute(array($page_no - 1, $limit));
+		$stat = $this->db->prepare("SELECT * FROM `flight_flight` ORDER BY `{$sort}` {$order} LIMIT :pos , :rows ;");
+		$stat->bindValue(":pos", ($page_no - 1) * 10, PDO::PARAM_INT);
+		$stat->bindValue(":rows", $limit, PDO::PARAM_INT);
+		$stat->execute();
 
 		return $stat->fetchAll(PDO::FETCH_ASSOC);
 	}
 	
 	public function get_page_count() {
-		return $this->db->query("SELECT COUNT(*) FROM `flight_flight` ;")->fetchColumn;
+		return $this->db->query("SELECT COUNT(*) FROM `flight_flight` ;")->fetchColumn();
 	}
 }
 ?>
