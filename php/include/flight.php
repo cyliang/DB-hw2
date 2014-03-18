c185ef954ec24afbf909023416f3a302b9539db2
Chih-Yung Liang <stu89424@gmail.com>
2014-03-18 12:54:48 +0800

    Add flight.php with class Flight
    which is used to query flight information.

---
+++ php/include/flight.php
@@ -0,0 +1,24 @@
+<?php
+class Flight {
+	private $db;
+
+	public function __construct($db) {
+		$this->db = $db;
+	}
+
+	public function get_page($page_no = 1, $options = array()) {
+		$order = (isset($options['descend']) && $options['descend'] === true) ? "DESC" : "ASC";
+		$sort = (isset($options['sort']) && $options['sort'] == 'date') ? "departure_date" : "id";
+		$limit = isset($options['limit']) ? $options['limit'] : 10;
+
+		$stat = $this->db->prepare("SELECT * FROM `flight_flight` ORDER BY `{$sort}` {$order} LIMIT ? , ? ;");
+		$stat->execute(array($page_no - 1, $limit));
+
+		return $stat->fetchAll(PDO::FETCH_ASSOC);
+	}
+	
+	public function get_page_count() {
+		return $this->db->query("SELECT COUNT(*) FROM `flight_flight` ;")->fetchColumn;
+	}
+}
+?>
