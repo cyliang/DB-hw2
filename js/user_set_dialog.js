6e26c4db43156a14fca721c315da8d5e660a8187
Chih-Yung Liang <liangcy@cs.nctu.edu.tw>
2014-03-21 09:56:08 +0800

    Finish the uset form

--- js/user_set_dialog.js
+++ js/user_set_dialog.js
@@ -5,7 +5,15 @@ function prepare_uset() {
 }
 
 function uset_onEnter() {
+	$("#uset #uset_rname input").attr("placeholder", "姓名：" + user.name);
+	$("#uset #uset_email input").attr("placeholder", "Email：" + user.email);
+	if(user.is_admin == 1) {
+		$("#uset #uset_admin input").attr("checked", "");
+	} else {
+		$("#uset #uset_admin input").removeAttr("checked");
+	}
 }
 
 function reset_uset() {
+	$("#uset input[type!=checkbox]").val("");
 }
